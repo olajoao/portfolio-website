e044faa52c6e2fdca42ac0c11d38eaf824174247
Joao Kuster <kusterzito@gmail.com>
2026-02-13 15:25:39 -0300

    chore: updated site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://joaokuster.dev',
+  site: 'https://joaokuster.vercel.app',
   integrations: [sitemap()],
 });
